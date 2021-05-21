84de34faf5ee527625e6df57163e3cf8716f9f8e
Josh <joshrasmussen34@gmail.com>
2021-05-21 12:44:27 -0700

    feat: initial commit

---
+++ workspace/site/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    siteMetadata: {
+        title: `Personal website of Josh Hamill`,
+        description: `Find blog posts and portfolio information here`,
+        author: `Josh Hamill`
+    },
+    plugins: [
+        `gatsby-plugin-pnpm`
+    ]
+}
---
+++ workspace/site/src/pages/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const IndexPage = () => (
+    <div>Hello world</div>
+)
+
+export default IndexPage
