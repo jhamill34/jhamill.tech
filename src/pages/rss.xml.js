5ef574f6202fa44b5fd11c91b08bbb5c38b61198
Josh Hamill <joshua@transposit.com>
2024-05-06 21:24:11 -0400

    feat: migrate to using collections

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(ctx) {
+    const posts = await getCollection("posts");
+
     return rss({
         title: "Astro Learner | Blog",
         description: "A blog about Astro",
         site: ctx.site,
-        items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/posts/${post.slug}/`,
+        })),
         customData: "<language>en</language>",
     });
 }
