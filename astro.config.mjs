9dc558a6bc180a60074ff50390e94588e3bc0104
Josh Hamill <joshua@transposit.com>
2024-05-08 16:27:47 -0400

    feat: tweak code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,25 @@
 import { defineConfig } from 'astro/config';
+import {
+  transformerNotationDiff,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from "@shikijs/transformers";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.jhamill.tech",
-  integrations: []
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-mocha',
+      wrap: true,
+      transformers: [
+        transformerNotationDiff(),
+        transformerNotationHighlight(),
+        transformerNotationWordHighlight(),
+      ],
+    }
+  }
 });
