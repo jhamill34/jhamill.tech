7767525fb72b792e8c12172c7d352bf255706e49
Josh Hamill <joshua@transposit.com>
2024-05-16 14:21:39 -0400

    Revert "feat: add amplitude tags"
    This reverts commit 9bd13eb61322855cc3a099347058d9e822e2961f.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface Window {
-    amplitude: any;
-    sessionReplay: any;
-    amplitudeAutocapturePlugin: any;
-}
-
