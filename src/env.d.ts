9bd13eb61322855cc3a099347058d9e822e2961f
Josh Hamill <joshua@transposit.com>
2024-05-16 14:13:29 -0400

    feat: add amplitude tags

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+    amplitude: any;
+    sessionReplay: any;
+    amplitudeAutocapturePlugin: any;
+}
+
